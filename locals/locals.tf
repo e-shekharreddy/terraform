9bf933484c03b81932f636be2238d22d53c4d218
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-18 00:12:28 +0530

    terraform

--- locals/locals.tf
+++ locals/locals.tf
@@ -6,5 +6,5 @@ locals {
         Terraform = "true"
     }
     ec2_final_tags = merge(local.common_tags, var.ec2_tags)
-    ami_id = "ami-0220d79f3f480ecf5"
+    ami_id = data.aws_ami.joindevops.id
 }
