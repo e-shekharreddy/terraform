31b9e222e968ecd93a2b4b73bd24e8faa223f683
Shekhar Reddy E <shekharreddy143s@gmail.com>
2026-03-30 21:52:34 +0530

    terraform

---
+++ import/main.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "import_demo" {
+    ami             = "ami-0220d79f3f480ecf5"
+    security_groups     =   [
+                            "allow all"
+                            ]
+    instance_type       = "t3.micro"
+    tags                  = {
+                            Name = "import"
+                            }
+    subnet_id           = "subnet-046cf30f2a124274f"
+
+    vpc_security_group_ids = [
+                            "sg-078ee4211bd66ef16"
+                             ]
+
+}
